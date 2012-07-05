3bcb5b9e1eed069fc2244d873ac181d5d68e956c
Far McKon <FarMcKon@gmail.com>
2012-07-05 16:42:35 -0400

    added base functionality of scanning flickr, secret.py is *not checked in*. You will need your own.

--- processing/fetch_flickr_data.py
+++ processing/fetch_flickr_data.py
@@ -1,6 +1,22 @@
 
-EVENT_NAME='pretest'
-SRC_ACCT='friskolator'
-API_KEY='Don't check me in!'
+import secret # import local secret.py file containing keys.
+	      # we expect 2 values in secret.py 
+	      # api_key = "XXXX" #< your flickr api key
+	      # api_secret = "YYYY" #< your flickr api secret
+
+import stream # we expect 2 id's 'user_id' and event tag
+	      # user_id = "USER_ID_ON_FLICKR"
+	      # event_tag = "tag to seach flickr for. 
+
+import flickrapi
+
+
+flickr = flickrapi.FlickrAPI(secret.api_key)
+
+filter_tags = [stream.event_tag, "friskolator"] # so we don't get general event tags
+
+for photo in flickr.walk(tag_mode="and",tags=filter_tags):
+    print photo.get('title')
+
 
 
---
+++ processing/stream.py
@@ -0,0 +1,5 @@
+#defines info for collecting data from flickr
+user_id = "81953314@N02"
+event_tag = "hopenumbernine"
+
+
